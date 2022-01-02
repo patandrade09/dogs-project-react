3d6e2b91e6535fe0ec1cbd7b697a127d56960892
patricia andrade <patricia.andrade094@gmail.com>
2022-01-02 05:21:35 -0300

    commit inicial repo estudo

---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Footer.module.css'
+
+const Footer = () => {
+    return (
+        <div className={styles.footer}>
+            Footer ta funfano
+        </div>
+    )
+}
+
+export default Footer
